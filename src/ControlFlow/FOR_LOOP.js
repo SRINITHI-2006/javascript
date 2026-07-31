cd1c5ac9d097fb77adb73c809f26e47c84b62bdc
SRINITHI-2006 <srinithisrinithi459@gmail.com>
2026-07-31 22:20:33 +0530

    Create FOR_LOOP.js

---
+++ src/ControlFlow/FOR_LOOP.js
@@ -0,0 +1,13 @@
+//Program: Multiplication Table of 5
+for (let i = 1; i <= 10; i++) {
+    console.log("5 x " + i + " = " + (5 * i));
+}
+
+//output
+/*
+5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15
+...
+5 x 10 = 50
+*/
