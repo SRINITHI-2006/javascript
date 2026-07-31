a95b88709d6dae3137e76d1477d15ab47a1351ab
SRINITHI-2006 <srinithisrinithi459@gmail.com>
2026-07-31 22:16:15 +0530

    Update IF_ELSE_Statement.js

--- src/ControlFlow/IF_ELSE_Statement.js
+++ src/ControlFlow/IF_ELSE_Statement.js
@@ -1,8 +1,12 @@
-//Program: Check Even or Odd
-let number = 15;
+//Program: Student Grade
+let marks = 82;
 
-if (number % 2 === 0) {
-    console.log("Even Number");
+if (marks >= 90) {
+    console.log("Grade A");
+} else if (marks >= 75) {
+    console.log("Grade B");
+} else if (marks >= 50) {
+    console.log("Grade C");
 } else {
-    console.log("Odd Number");
+    console.log("Fail");
 }
