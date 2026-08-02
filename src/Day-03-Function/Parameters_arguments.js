c6f3837df2d270765647d3c64ced3306868aa62f
SRINITHI-2006 <srinithisrinithi459@gmail.com>
2026-08-02 22:03:47 +0530

    Add Parameters and Arguments explanation in JS

---
+++ src/Day-03-Function/Parameters_arguments.js
@@ -0,0 +1,8 @@
+/*
+Parameter is a variable written inside the function definition.
+Argument is the actual value passed to the function when it is called.
+*/
+function greet(name) {       // name = parameter
+   console.log("Hello, " + name);
+ }
+ greet("Alex");                // "Alex" = argument
