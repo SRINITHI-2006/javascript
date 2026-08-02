e9b724aa40eed57ed1b51b4ace804af8d836aa23
SRINITHI-2006 <srinithisrinithi459@gmail.com>
2026-08-02 22:06:13 +0530

    Add function to return the sum of two numbers

---
+++ src/Day-03-Function/Return_Values.js
@@ -0,0 +1,11 @@
+/*
+The return statement sends a value back from the function.
+Without return, the function returns undefined.
+*/
+
+function add(a, b) 
+{ 
+  return a + b; 
+} 
+let result = add(10, 20); 
+console.log(result);
