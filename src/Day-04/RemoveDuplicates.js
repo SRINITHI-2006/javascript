af35a625bf572a49c81105553da23c3df194cae5
SRINITHI-2006 <srinithisrinithi459@gmail.com>
2026-08-03 22:05:08 +0530

    Add function to remove duplicates from an array

---
+++ src/Day-04/RemoveDuplicates.js
@@ -0,0 +1,11 @@
+function removeDuplicates(arr) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!result.includes(arr[i])) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
+let numbers = [1, 2, 2, 3, 4, 4, 5];
+console.log(removeDuplicates(numbers));
