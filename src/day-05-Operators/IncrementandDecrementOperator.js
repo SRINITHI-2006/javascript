b4e472b5fb6baf07a1b2fd63405dce0cd52d64fe
SRINITHI-2006 <srinithisrinithi459@gmail.com>
2026-08-04 22:15:25 +0530

    Create  IncrementandDecrementOperator.js

---
+++ src/day-05-Operators/IncrementandDecrementOperator.js
@@ -0,0 +1,13 @@
+/*
+For the increment operator, 
+    prefix is ++x  &
+    postfix is x++.
+*/
+// prefix
+let x = 5;
+console.log(++x); // 6
+console.log(x); // 6
+//postfix
+let y = 5;
+console.log(y++); // 5
+console.log(y); // 6
